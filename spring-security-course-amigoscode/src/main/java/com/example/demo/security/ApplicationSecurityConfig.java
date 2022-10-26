7333e252392a3b5025481d9d596824e437236010
Lorenzo Pacitto <lorenzo.pacitto97@gmail.com>
2022-10-26 16:02:52 +0200

    Added antMatchers and authorization

--- spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -27,9 +29,24 @@ public class ApplicationSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz ->
-                authz.anyRequest().authenticated()
-            ).httpBasic(withDefaults());
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(withDefaults()).formLogin();
+//        http
+//                .authorizeHttpRequests()
+//                .antMatchers("/", "index.html", "/css/*", "/js/")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic(withDefaults()).formLogin();
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/", "index", "/css/*", "/js/*");
     }
 }
