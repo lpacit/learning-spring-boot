a703a7cd6ccaef31ac1ea3f9c5b6e46ac731ea94
Lorenzo Pacitto <lorenzo.pacitto97@gmail.com>
2022-10-27 15:38:08 +0200

    Removed unused imports

--- spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
