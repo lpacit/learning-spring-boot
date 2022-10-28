bebe149672a7f118ddcb5a6bdd9a25e102159605
Lorenzo Pacitto <lorenzo.pacitto97@gmail.com>
2022-10-28 15:46:05 +0200

    added @PreAuthorized annotation

--- spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -2,19 +2,20 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.demo.security.ApplicationUserRole.*;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig {
 
     @Bean
@@ -25,14 +26,10 @@ public class ApplicationSecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                         .antMatchers("/api/**").hasRole(STUDENT.name())
-
-                        // Here we are basically saying that DELETE, POST and PUT can be done by someone with COURSE_WRITE permission
-                        .antMatchers(HttpMethod.DELETE, managementAntMatcherPath).hasAuthority(COURSE_WRITE.getPermission())
-                        .antMatchers(HttpMethod.POST, managementAntMatcherPath).hasAuthority(COURSE_WRITE.getPermission())
-                        .antMatchers(HttpMethod.PUT, managementAntMatcherPath).hasAuthority(COURSE_WRITE.getPermission())
-
-                        // Here we are saying that this path is restricted to ADMIN and ADMINTRAINEE
-                        .antMatchers(HttpMethod.GET, managementAntMatcherPath).hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+//                        .antMatchers(HttpMethod.DELETE, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission()) // Here we are basically saying that DELETE, POST and PUT can be done by someone with STUDENT_WRITE permission
+//                        .antMatchers(HttpMethod.POST, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission())
+//                        .antMatchers(HttpMethod.PUT, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission())
+//                        .antMatchers(HttpMethod.GET, managementAntMatcherPath).hasAnyRole(ADMIN.name(), ADMINTRAINEE.name()) // Here we are saying that this path is restricted to ADMIN and ADMINTRAINEE
                         .anyRequest()
                         .authenticated()
                 )
@@ -46,21 +43,18 @@ public class ApplicationSecurityConfig {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("annasmith"))
-//                .roles(STUDENT.name())   // ROLE_STUDENT
                 .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("linda"))
-//                .roles(ADMIN.name())
                 .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("tom"))
-//                .roles(ADMINTRAINEE.name())
                 .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
--- spring-security-course-amigoscode/src/main/java/com/example/demo/student/ManagementStudentController.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/student/ManagementStudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,24 +17,29 @@ public class ManagementStudentController {
     );
 
     @GetMapping
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
+    @PreAuthorize("hasAuthority('student:read')")
     public List<Student> getAllStudent() {
         System.out.println("getAllStudent");
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student newStudent) {
         System.out.println("registerNewStudent");
         System.out.println(newStudent);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println("updateStudent");
         System.out.printf("%s %s%n", studentId, student);
