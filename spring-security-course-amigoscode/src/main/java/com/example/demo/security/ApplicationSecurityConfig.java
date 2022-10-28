9b5faaa8692e105318406df2edc9859e20adef75
Lorenzo Pacitto <lorenzo.pacitto97@gmail.com>
2022-10-28 14:43:10 +0200

    added permission based authentication

--- spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.demo.security.ApplicationUserRole.*;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,13 +19,23 @@ public class ApplicationSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        String managementAntMatcherPath = "/management/api/**";
         http
                 .csrf().disable()
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                         .antMatchers("/api/**").hasRole(STUDENT.name())
+
+                        // Here we are basically saying that DELETE, POST and PUT can be done by someone with COURSE_WRITE permission
+                        .antMatchers(HttpMethod.DELETE, managementAntMatcherPath).hasAuthority(COURSE_WRITE.getPermission())
+                        .antMatchers(HttpMethod.POST, managementAntMatcherPath).hasAuthority(COURSE_WRITE.getPermission())
+                        .antMatchers(HttpMethod.PUT, managementAntMatcherPath).hasAuthority(COURSE_WRITE.getPermission())
+
+                        // Here we are saying that this path is restricted to ADMIN and ADMINTRAINEE
+                        .antMatchers(HttpMethod.GET, managementAntMatcherPath).hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                         .anyRequest()
-                        .authenticated())
+                        .authenticated()
+                )
                 .httpBasic(withDefaults()).formLogin();
 
         return http.build();
@@ -34,19 +46,22 @@ public class ApplicationSecurityConfig {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("annasmith"))
-                .roles(STUDENT.name())   // ROLE_STUDENT
+//                .roles(STUDENT.name())   // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("linda"))
-                .roles(ADMIN.name())
+//                .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("tom"))
-                .roles(ADMINTRAINEE.name())
+//                .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
--- spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationUserRole.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -1,8 +1,11 @@
 package com.example.demo.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.demo.security.ApplicationUserPermission.*;
 
@@ -13,12 +16,21 @@ public enum ApplicationUserRole {
 
     private final Set<ApplicationUserPermission> permissions;
 
-
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
         return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> authorities = getPermissions().stream()
+                .map((ApplicationUserPermission permission) -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+
+        return authorities;
     }
 }
