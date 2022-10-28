579c39192d3f2b60fd91a821091c77a20b15ea93
Lorenzo Pacitto <lorenzo.pacitto97@gmail.com>
2022-10-28 16:24:59 +0200

    commented code

--- spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -20,7 +20,7 @@ public class ApplicationSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        String managementAntMatcherPath = "/management/api/**";
+//        String managementAntMatcherPath = "/management/api/**";
         http
                 .csrf().disable()
                 .authorizeHttpRequests(authorize -> authorize
