d95fd7d86ea40ad2bdff6e1a6fb4c08083acaeb5
lpacit <lorenzo.pacitto97@gmail.com>
2022-11-07 17:25:44 +0100

    Update JwtUsernameAndPasswordAuthenticationFilter.java

--- spring-security-course-amigoscode/src/main/java/com/example/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.example.demo.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -13,17 +15,35 @@ import java.io.IOException;
  * Verifies the credentials it is given.
  * */
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    /**
+     * Not Autowiring because we will use it in ApplicationSecurityConfig.
+     * */
+    private final AuthenticationManager authenticationManager;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    /**
+     * This method is the SERVER part of the image in the README.md, which is given the credentials and validates them.
+     * */
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
         try {
-            UsernamePasswordAuthenticationFilter authenticationRequest = new ObjectMapper()
-                    .readValue(request.getInputStream(), UsernamePasswordAuthenticationFilter.class);
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
+                    .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()
+            );
+
+            return authenticationManager.authenticate(authentication);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
-        return super.attemptAuthentication(request, response);
     }
 }
