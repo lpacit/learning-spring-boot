a0c19bb86c7e708454b8e309d4e74dc0ad57c7b5
lpacit <lorenzo.pacitto97@gmail.com>
2022-11-07 15:43:28 +0100

    Update ApplicationSecurityConfig.java

--- spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,14 +1,14 @@
 package com.example.demo.security;
 
+import com.example.demo.auth.ApplicationUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -22,18 +22,35 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig {
 
+    private final ApplicationUserService applicationUserService;
+
+    public ApplicationSecurityConfig(ApplicationUserService applicationUserService) {
+        this.applicationUserService = applicationUserService;
+    }
+
+    /**
+     * If you don't want to use @PreAuthorize annotations, use this code:
+     * String managementAntMatcherPath = "/management/api/**";
+     *
+     * http
+     *     .csrf().disable()
+     *     .authorizeHttpRequests(authorize -> authorize
+     *          .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+     *          .antMatchers("/api/**").hasRole(STUDENT.name())
+     *     .antMatchers(HttpMethod.DELETE, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission())
+     *     .antMatchers(HttpMethod.POST, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission())
+     *     .antMatchers(HttpMethod.PUT, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission())
+     *     .antMatchers(HttpMethod.GET, managementAntMatcherPath).hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+     *     ...)
+     *     ...
+     * */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        String managementAntMatcherPath = "/management/api/**";
         http
                 .csrf().disable()
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                         .antMatchers("/api/**").hasRole(STUDENT.name())
-//                        .antMatchers(HttpMethod.DELETE, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission()) // Here we are basically saying that DELETE, POST and PUT can be done by someone with STUDENT_WRITE permission
-//                        .antMatchers(HttpMethod.POST, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission())
-//                        .antMatchers(HttpMethod.PUT, managementAntMatcherPath).hasAuthority(STUDENT_WRITE.getPermission())
-//                        .antMatchers(HttpMethod.GET, managementAntMatcherPath).hasAnyRole(ADMIN.name(), ADMINTRAINEE.name()) // Here we are saying that this path is restricted to ADMIN and ADMINTRAINEE
                         .anyRequest()
                         .authenticated()
                 )
@@ -57,30 +74,41 @@ public class ApplicationSecurityConfig {
         return http.build();
     }
 
+    /**
+     * Before implemented like this:
+     *     @ Bean
+     *     protected InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+     *         UserDetails annaSmithUser = User.builder()
+     *                 .username("annasmith")
+     *                 .password(passwordEncoder.encode("annasmith"))
+     *                 .authorities(STUDENT.getGrantedAuthorities())
+     *                 .build();
+     *         UserDetails lindaUser = User.builder()
+     *                 .username("linda")
+     *                 .password(passwordEncoder.encode("linda"))
+     *                 .authorities(ADMIN.getGrantedAuthorities())
+     *                 .build();
+     *         UserDetails tomUser = User.builder()
+     *                 .username("tom")
+     *                 .password(passwordEncoder.encode("tom"))
+     *                 .authorities(ADMINTRAINEE.getGrantedAuthorities())
+     *                 .build();
+     *         return new InMemoryUserDetailsManager(
+     *                 annaSmithUser,
+     *                 lindaUser,
+     *                 tomUser
+     *         );
+     *     }
+     * */
+    protected void configure(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider(passwordEncoder));
+    }
+
     @Bean
-    protected InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails annaSmithUser = User.builder()
-                .username("annasmith")
-                .password(passwordEncoder.encode("annasmith"))
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-
-        UserDetails lindaUser = User.builder()
-                .username("linda")
-                .password(passwordEncoder.encode("linda"))
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        UserDetails tomUser = User.builder()
-                .username("tom")
-                .password(passwordEncoder.encode("tom"))
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(
-                annaSmithUser,
-                lindaUser,
-                tomUser
-        );
+    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
     }
 }
