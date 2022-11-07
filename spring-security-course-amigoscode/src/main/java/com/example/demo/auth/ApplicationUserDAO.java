61f02e7cbebedd8984c7223b07333b93417e05b3
lpacit <lorenzo.pacitto97@gmail.com>
2022-11-07 12:22:43 +0100

    Update ApplicationUserDAO.java

--- spring-security-course-amigoscode/src/main/java/com/example/demo/auth/ApplicationUserDAO.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/auth/ApplicationUserDAO.java
@@ -3,7 +3,5 @@ package com.example.demo.auth;
 import java.util.Optional;
 
 public interface ApplicationUserDAO {
-
     Optional<ApplicationUser> selectApplicationUserByUsername(String username);
-
 }
