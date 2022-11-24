63405b5308ec99f6542505be66c0f26a8fa97985
Lorenzo Pacitto <lorenzo.pacitto97@gmail.com>
2022-11-24 18:40:44 +0100

    provide authentication with token

--- spring-security-course-amigoscode/src/main/java/com/example/demo/customDSL/MyCustomDsl.java
+++ spring-security-course-amigoscode/src/main/java/com/example/demo/customDSL/MyCustomDsl.java
@@ -1,18 +1,21 @@
 package com.example.demo.customDSL;
 
+import com.example.demo.jwt.JwtTokenVerifier;
 import com.example.demo.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-public class MyCustomDslForAuthenticationManager extends AbstractHttpConfigurer<MyCustomDslForAuthenticationManager, HttpSecurity> {
+public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
-        http.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager));
+        http
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager))
+                .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class);
     }
 
-    public static MyCustomDslForAuthenticationManager customDsl() {
-        return new MyCustomDslForAuthenticationManager();
+    public static MyCustomDsl customDsl() {
+        return new MyCustomDsl();
     }
 }
