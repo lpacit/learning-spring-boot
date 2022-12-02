82e955f7ea84a276a0cd747fa6fe3a21bf685edb
Lorenzo Pacitto <lorenzo.pacitto97@gmail.com>
2022-12-02 10:57:39 +0100

    Update WorkbookController.java

--- udemy-course/02. model-view-controller/workbook 2.2/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ udemy-course/02. model-view-controller/workbook 2.2/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -1,13 +1,26 @@
 package com.ltp.workbook;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class WorkbookController {
     
     @GetMapping("/")
-    public String getShows() {
+    public String getShows(Model model) {
+        model.addAllAttributes(List.of(
+                new Show("Breaking Bad", "Ozymandias", 10),
+                new Show("Attack on Titan", "Hero", 9.9),
+                new Show("Attack on Titan", "Perfect Game", 9.9),
+                new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9),
+                new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9)
+        ));
         return "shows";
     }
 
